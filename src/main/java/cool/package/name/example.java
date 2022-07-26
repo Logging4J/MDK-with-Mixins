e6cf3584ebbbe9ddc81cdfb3dd4da6b978f31335
Logging4J <80828850+Logging4J@users.noreply.github.com>
2022-07-26 10:33:15 -0400

    i forgot

---
+++ src/main/java/cool/package/name/example.java
@@ -0,0 +1,10 @@
+package sexy.l4j.dickcord;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(   modid = "example",
+        name = "Example",
+        version = "0.0.1"
+)
+public class Main {}
